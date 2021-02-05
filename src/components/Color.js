9c5589b5c80e0e42cc9cdc7f6c23890492e29915
m0ohannad <mr.m0ohannad@gmail.com>
2021-02-06 01:50:38 +0300

    project4

--- src/components/Color.js
+++ src/components/Color.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Color = (props) => {
     return (
-        <label style={{background: props.color}} for={props.name} ><input type="radio" name="color" value={props.name}/></label>
+        <label style={{background: props.color}} htmlFor={props.name} ><input type="radio" name="color" value={props.name}/></label>
     );
 }
 
--- src/components/Details.js
+++ src/components/Details.js
@@ -6,28 +6,30 @@ const Details = (props) => {
     return (
         <div className="container">
             <div className="details">
-                <div>
-                    <img className="small-img" src={props.img} width="5%" alt={props.name} />
-                    <img className="small-img" src={props.img} width="5%" alt={props.name} />
-                    <img className="small-img" src={props.img} width="5%" alt={props.name} />
+                <div className="column">
+                    <img className="small-img" src={props.img} width="100%" alt={props.name} />
+                    <img className="small-img" src={props.img} width="100%" alt={props.name} />
+                    <img className="small-img" src={props.img} width="100%" alt={props.name} />
                 </div>
-                <img className="details-img" src={props.img} width="50%" alt={props.name} />
-                <div className="text">
-                    <h2>{props.price}</h2>
-                    <h2>{props.name}</h2>
-                    <p>{props.description}</p>
-                </div>
-                <div className="choice">
-                    <h5><b>Color: </b> {props.selected}</h5> 
-                    <div className="colors">
-                        <Color color="#D07071" name="red" />
-                        <Color color="#7074D0" name="blue" />
-                        <Color color="#333333" name="black" />
+                <img className="details-img" src={props.img} width="100%" alt={props.name} />
+                <div className="column content">
+                    <div className="text">
+                        <h2>{props.price}</h2>
+                        <h2>{props.name}</h2>
+                        <p>{props.description}</p>
                     </div>
-                </div>
-                <div className="add">
-                    <input id="number" type="number" />
-                    <Button text="Add to cart" />
+                    <div className="choice">
+                        <h5>Color: </h5><p>{props.selected}</p>
+                        <div className="colors">
+                            <Color color="#D07071" name="red" />
+                            <Color color="#7074D0" name="blue" />
+                            <Color color="#333333" name="black" />
+                        </div>
+                    </div><br/>
+                    <div className="add">
+                        <input id="number" type="number" />
+                        <Button text="Add to cart" />
+                    </div>
                 </div>
             </div>
         </div>
